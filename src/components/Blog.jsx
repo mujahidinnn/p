4c09cd4e23e0a1ef5df0ec77926c221a0860e0fd
Mujahidin1802 <mujahidin28394@gmail.com>
2023-01-23 19:22:40 +0700

    edit padding in file blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -13,11 +13,11 @@ const Blog = () => {
       <p className="pl-3 text-left text-gray-600 md:text-center">
         My note collection
       </p>
-      <div className="grid grid-cols-1 gap-4 p-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 p-2 md:p-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         {data.map((data) => {
           return (
             <>
-              <div className=" flex h-40 w-full rounded bg-white/10  lg:h-48 lg:max-w-full">
+              <div className="flex h-40 w-full rounded bg-white/10  lg:h-48 lg:max-w-full">
                 <div
                   className="h-40 w-32 flex-none overflow-hidden rounded-l bg-cover text-center lg:h-auto lg:w-48"
                   title={data.name}
@@ -26,14 +26,14 @@ const Blog = () => {
                   <img src={data.image} alt={data.name} className="h-full" />
                 </div>
                 <div
-                  className="flex flex-col justify-between rounded-r bg-[#051D2C] p-4 leading-3  lg:border-gray-400"
+                  className="flex flex-col justify-between rounded-r bg-[#051D2C] p-2 md:p-4 leading-3  lg:border-gray-400"
                   data-aos="flip-down"
                 >
                   <div className="mb-8">
-                    <div className="mb-2 text-[18px] font-semibold text-cyan-600 md:text-xl leading-4">
+                    <div className="mb-1 text-[18px] font-semibold text-cyan-600 md:text-xl leading-4">
                       {data.name}
                     </div>
-                    <p className="leading-2 mb-3 text-xs text-gray-500 lg:text-base flex-nowrap flex">
+                    <p className="leading-2 mb-2 text-xs text-gray-500 lg:text-base flex-nowrap flex">
                       {data.desc.substring(0,90)}...
                     </p>
                     <a
--- src/database/dataBlog.jsx
+++ src/database/dataBlog.jsx
@@ -1,19 +1,19 @@
 const data = [
   {
     name: "Fundamental React Js",
-    desc: "What is ReactJs? React is a JavaScript library for building user interfaces created by Facebook in 2013. React is not a framework for creating UIs but a library for build a UI on the website...",
+    desc: "What is ReactJs? React is a JavaScript library for building user interfaces created by Facebook in 2013. React is not a framework for creating UIs but a library for build a UI on the website",
     image: "https://miro.medium.com/max/720/0*VjSPn5B6XlQydYAC",
     link: "https://medium.com/@mujahidin28394/fundamental-react-js-for-beginner-f6c58c4f9342",
   },
   {
     name: "Props vs State",
-    desc: "State dan props pada ReactJS tidak seperti Lib/framework lainnya, ReactJS datang dengan sesuatu yang unik tidak seperti bahasa pemrograman/framework lain...",
+    desc: "State dan props pada ReactJS tidak seperti Lib/framework lainnya, ReactJS datang dengan sesuatu yang unik tidak seperti bahasa pemrograman/framework lain",
     image: "https://miro.medium.com/max/4800/1*gCfSTMaxWyM_uiej4QGKeg.webp",
     link: "https://medium.com/@mujahidin28394/props-dan-state-9bb50b9bccf0",
   },
   {
     name: "Sekilas ReactJS",
-    desc: "Class component dan Functional component sama-sama memiliki fungsi untuk membuat component pada ReactJS. Namun ,ada perbedaan antara...",
+    desc: "Class component dan Functional component sama-sama memiliki fungsi untuk membuat component pada ReactJS. Namun ,ada perbedaan antara",
     image: "https://miro.medium.com/max/720/0*plrOWRUeTRTH9Dnc.webp",
     link: "https://medium.com/@mujahidin28394/%EF%BD%81%EF%BD%92%EF%BD%94%EF%BD%89%EF%BD%8B%EF%BD%85%EF%BD%8C-%EF%BD%81%EF%BD%8E%EF%BD%81%EF%BD%8C%EF%BD%89%EF%BD%93%EF%BD%81-7ab7a468cb30",
   },
